9f1ad03036e91b724c2087f7ca114f2a72be4f0d
drherobrine <tim.melentyev@gmail.com>
2017-11-21 17:15:47 +0100

    Initial commit; added basic movement test

---
+++ README.txt
@@ -0,0 +1,4 @@
+
+
+Pug Life™ is a trademark of MeGoRoo & co.
+Copyright (c) 2017 MeGoRoo and company
---
+++ src/movement.gd
@@ -0,0 +1,24 @@
+extends TestCube
+
+var speed = 0
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+	
+func _process(delta):
+	if Input.is_action_pressed("move_forward"):
+		speed = -8
+	elif Input.is_action_pressed("move_back"):
+		speed = 8
+	elif Input.is_action_pressed("move_left"):
+		self.rotate_y(-0.0625)
+	elif Input.is_action_pressed("move_right"):
+		self.rotate_y(0.0625)
+	else:
+		speed = 0
+	translate(Vector3(0,0,delta * speed))
